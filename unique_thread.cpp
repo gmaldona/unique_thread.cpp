a28322ee0cd313f6c4d05fe028a156ed082b0ceb
Gregory Maldonado <gmaldona@oswego.edu>
2023-10-19 20:59:00 -0400

    Add files via upload

--- unique_thread.cpp
+++ unique_thread.cpp
@@ -2,38 +2,29 @@
 
 using namespace orange;
 
-std::mutex* unique_thread::lock = new std::mutex();
+vex::mutex *unique_thread::lock = new vex::mutex();
 func_ptr unique_thread::_callback = nullptr;
 
 unique_thread::unique_thread() {
-	lock = new std::mutex();
-	_callback = nullptr;
+   lock = new vex::mutex();
+   _callback = nullptr;
 }
 
-unique_thread::~unique_thread() {
-	delete orange::unique_thread::lock;
-}
+unique_thread::~unique_thread() { delete unique_thread::lock; }
 
-std::mutex* unique_thread::get_lock() {
-	return lock;
-}
+vex::mutex *unique_thread::get_lock() { return lock; }
 
-func_ptr unique_thread::get_callback() {
-	return _callback;
-}
+func_ptr unique_thread::get_callback() { return _callback; }
 
-void unique_thread::set_callback(orange::func_ptr callback) {
-	_callback = callback;
-}
+void unique_thread::set_callback(func_ptr callback) { _callback = callback; }
 
-void orange::unique_thread::execute(func_ptr callback)
-{
-	if (orange::unique_thread::get_lock()->try_lock()) {
-		orange::unique_thread::set_callback(callback);
-		std::thread t([]() {
-		  orange::unique_thread::get_callback()();
-		  orange::unique_thread::get_lock()->unlock();
-		});
-		t.detach();
-	}
+void unique_thread::execute(func_ptr callback) {
+   if (unique_thread::get_lock()->try_lock()) {
+      unique_thread::set_callback(callback);
+      vex::thread t([]() {
+         unique_thread::get_callback()();
+         unique_thread::get_lock()->unlock();
+      });
+      t.detach();
+   }
 }
