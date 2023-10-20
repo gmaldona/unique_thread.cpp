6e9d657babaedc25c2ce0fcf97239ee130284d63
Gregory Maldonado <gmaldona@oswego.edu>
2023-10-19 21:39:44 -0400

    smart pointers over raw pointer

--- unique_thread.cpp
+++ unique_thread.cpp
@@ -2,29 +2,26 @@
 
 using namespace orange;
 
-vex::mutex *unique_thread::lock = new vex::mutex();
+std::unique_ptr<std::mutex> unique_thread::lock(new std::mutex());
 func_ptr unique_thread::_callback = nullptr;
 
-unique_thread::unique_thread() {
-   lock = new vex::mutex();
-   _callback = nullptr;
+unique_thread::unique_thread() = default;
+
+std::unique_ptr<std::mutex> &&unique_thread::get_lock() {
+  return std::move(lock);
 }
-
-unique_thread::~unique_thread() { delete unique_thread::lock; }
-
-vex::mutex *unique_thread::get_lock() { return lock; }
 
 func_ptr unique_thread::get_callback() { return _callback; }
 
 void unique_thread::set_callback(func_ptr callback) { _callback = callback; }
 
 void unique_thread::execute(func_ptr callback) {
-   if (unique_thread::get_lock()->try_lock()) {
-      unique_thread::set_callback(callback);
-      vex::thread t([]() {
-         unique_thread::get_callback()();
-         unique_thread::get_lock()->unlock();
-      });
-      t.detach();
-   }
+  if (unique_thread::get_lock()->try_lock()) {
+    unique_thread::set_callback(callback);
+    std::thread t([]() {
+      unique_thread::get_callback()();
+      unique_thread::get_lock()->unlock();
+    });
+    t.detach();
+  }
 }
--- unique_thread.h
+++ unique_thread.h
@@ -1,26 +1,27 @@
 #ifndef C__11TESTING__UNIQUE_THREAD_H
 #define C__11TESTING__UNIQUE_THREAD_H
 
-#include "vex.h"
+#include <memory>
+#include <mutex>
+#include <thread>
 
 namespace orange {
 typedef void (*func_ptr)(void);
 
 class unique_thread {
-  public:
-   unique_thread();
-   ~unique_thread();
+ public:
+  unique_thread();
 
-   static void execute(func_ptr callback);
+  static void execute(func_ptr callback);
 
-   static vex::mutex *get_lock();
+  static std::unique_ptr<std::mutex> &&get_lock();
 
-   static func_ptr get_callback();
-   static void set_callback(func_ptr callback);
+  static func_ptr get_callback();
+  static void set_callback(func_ptr callback);
 
-  private:
-   static vex::mutex *lock;
-   static func_ptr _callback;
+ private:
+  static std::unique_ptr<std::mutex> lock;
+  static func_ptr _callback;
 };
 
 }  // namespace orange
